206dbff821a0f41ccf96bcc61df8e8342a88f463
imlogang <37604784+imlogang@users.noreply.github.com>
2022-02-23 17:06:45 -0600

    uploading everything

---
+++ calculator.py
@@ -0,0 +1,6 @@
+def ConversionRate(Number1, Number2):
+
+        rate = Number1 + Number2
+    
+        return (rate)
+
---
+++ tests/test_calculator.py
@@ -0,0 +1,40 @@
+import unittest
+import random
+from calculator import ConversionRate
+
+class TestCalc(unittest.TestCase):
+    def test_int(self): 
+
+        Number1 = random.randint(0,1000000)
+        Number2 = random.randint(0,1000000)
+
+        ExpectedValue = Number1 + Number2
+
+        result = ConversionRate(Number1, Number2)
+
+        self.assertEqual(result, ExpectedValue)
+    
+    def test_float(self): 
+
+        Number1 = random.random()
+        Number2 = random.random()
+
+        ExpectedValue = Number1 + Number2
+
+        result = ConversionRate(Number1, Number2)
+
+        self.assertEqual(result, ExpectedValue)
+
+    def test_uniform(self): 
+
+        Number1 = random.uniform(0,1000000)
+        Number2 = random.uniform(0,1000000)
+
+        ExpectedValue = Number1 + Number2
+
+        result = ConversionRate(Number1, Number2)
+
+        self.assertEqual(result, ExpectedValue)
+
+
+    
